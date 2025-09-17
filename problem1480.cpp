798734618ee1ce708b632251336a034391b5574d
Mohit <mohitt.tech@gmail.com>
2025-09-17 12:21:09 +0530

    Update problem1480.cpp

---
+++ problem1480.cpp
@@ -0,0 +1,11 @@
+// leetCode problem 1480 - Running Sum of 1d Array
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+         int n = nums.size();
+         for(int i =1;i<n;i++){
+            nums[i] = nums[i-1] + nums[i];
+         }
+         return nums;
+    }
+};
